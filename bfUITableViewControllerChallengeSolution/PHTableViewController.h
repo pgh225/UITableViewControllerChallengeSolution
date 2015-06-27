f80763b45e440bf5dc01a3a244db7da5d89dc9d4
Patrick Healey <pgh225@nyu.edu>
2015-06-27 02:24:15 -0400

    solved the challenge

---
+++ bfUITableViewControllerChallengeSolution/PHTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  PHTableViewController.h
+//  bfUITableViewControllerChallengeSolution
+//
+//  Created by Patrick Healey on 6/27/15.
+//  Copyright (c) 2015 Pat Healey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PHTableViewController : UITableViewController
+
+@end

e55f9e0b2bdf0b95f89ee7e3f2c4a7ea2e4dfbe3
Patrick Healey <pgh225@nyu.edu>
2015-06-27 02:12:57 -0400

    created project

---
+++ bfUITableViewControllerChallengeSolution/PHViewController.h
@@ -0,0 +1,13 @@
+//
+//  PHViewController.h
+//  bfUITableViewControllerChallengeSolution
+//
+//  Created by Patrick Healey on 6/27/15.
+//  Copyright (c) 2015 Pat Healey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PHViewController : UIViewController
+
+@end
---
+++ bfUITableViewControllerChallengeSolution/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  bfUITableViewControllerChallengeSolution
+//
+//  Created by Patrick Healey on 6/27/15.
+//  Copyright (c) 2015 Pat Healey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "PHAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([PHAppDelegate class]));
+    }
+}
